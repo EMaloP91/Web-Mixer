77b46678db502324627b85b4f63fbc70fc83568f
Esteban <esteban.malo91@gmail.com>
2024-11-17 17:07:12 -0500

    added an array to store the URLs of the audio in order to access them later in with audio Web API

--- static/main.js
+++ static/main.js
@@ -23,15 +23,26 @@ faders.forEach(fader => {
     });
 });
 
+//declare array to store the URLs of the audio files
+let audioUrls = [];
+
 // add name of each instrument to every channel
 // fetch audio files from server
 fetch('/audiofiles')
+    // turn response of server to javascript objects
     .then(response => response.json())
+    // use the javascript objects obtained from server
     .then(audioFilenames => {
+        // get all nametrack <p> elements
         let names = document.querySelectorAll(".nametrack");
+        // loop through each name 
         names.forEach((name,index) => {
             if (index < audioFilenames.length) {
+                // change the name of the channel to each audiofile name
                 name.innerText = audioFilenames[index];
+                // store the urls of each sound in the array
+                let audioUrl = `/static/${audioFilenames[index]}`;
+                audioUrls.push(audioUrl);
             }
         });
     })
